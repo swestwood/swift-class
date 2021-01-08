c748e496c5865538b76f3a59fbc74300db49f13c
Sophia Westwood <swestwood92@gmail.com>
2021-01-07 17:47:32 -0800

    Initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sophia Westwood on 1/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
